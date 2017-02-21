b1ab05c80c439773a03b865afa95145993dfd33d
Victor Diaz de Leon <vrdiazdel@gmail.com>
2017-02-20 21:38:40 -0800

    Add CORS middleware

--- server/app.js
+++ server/app.js
@@ -13,6 +13,11 @@ mongoose.connect(dbConfig.url, () => {
 });
 
 // Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
