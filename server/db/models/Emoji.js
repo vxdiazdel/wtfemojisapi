455cfca1a682ffa0717ac60ee4979cbdd5d864c1
Victor Diaz de Leon <vrdiazdel@gmail.com>
2017-02-25 17:10:06 -0800

    Update mongoose Schema

--- server/db/models/Emoji.js
+++ server/db/models/Emoji.js
@@ -6,7 +6,19 @@ mongoose.Promise = global.Promise;
 const emojiSchema = new Schema({
   _id: Schema.Types.ObjectId,
   emoji: String,
-  name: String
+  data: {
+    "aliases_ascii": [],
+    "unicode": String,
+    "code_decimal": String,
+    "category": String,
+    "emoji_order": String,
+    "aliases": [],
+    "unicode_alt": String,
+    "name": String,
+    "shortname": String,
+    "keywords": []
+  },
+  count: Number
 });
 
 const Emoji = mongoose.model('Emoji', emojiSchema);
